b868849d87fdc6b50a4177db490e67a77a0b2293
MehdiDgrs <Mehdigital@outlook.com>
2022-07-08 14:06:59 +0200

    reset

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,52 +1,75 @@
 
-import sendgrid from "@sendgrid/mail";
+import nodemailer from 'nodemailer'
 
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
-
+export default (req,res) => {
   
  
-  
+    async function main() {
     
       
      
-    
-
-        
-
+      
        
-        
-        // send mail with defined transport object
-        async function sendEmail(req, res) {
-          let {Nom,Prenom,Email,Tel,Message} = req.body;
-          console.log(sendgrid.setApiKey('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'))
-          try {
-            // console.log("REQ.BODY", req.body);
-            await sendgrid.send({
-              from: 'Mehdigital@outlook.com', // sender address
-              to: 'Mehdigital@outlook.com', // list of receivers
-              subject: "Portfolio PHOTO", // Subject line
-              text: `Name:${Nom} Prenom:${Prenom} Email:${Email} Message:${Message} Tel:${Tel}`, // plain text body
-              html: `<span>Name: ${Nom}</span><br><span>Prenom: ${Prenom}</span><br><span>Email: ${Email}</span><br><span>Message: ${Message}</span></span><br><span>Tel: ${Tel}</span>`, // html body
-            });
-          } catch (error) {
-            // console.log(error);
-            return res.status(error.statusCode || 500).json({ error: error.message });
-          }
-       
-          return res.status(200).json({ error: "" });
+        let transporter = await nodemailer.createTransport({
+          host: "smtp-mail.outlook.com",
+          port: 587,
+          secure: false, // true for 465, false for other ports
+          auth: {
+            user: process.env.SMTP_MAIL, 
+            pass: process.env.SMTP_PASSWORD, 
+          },
+        });
+
+        await new Promise((resolve, reject) => {
+          // verify connection configuration
+          transporter.verify(function (error, success) {
+              if (error) {
+                  console.log(error);
+                  reject(error);
+              } else {
+                  console.log("Server is ready to take our messages");
+                  resolve(success);
+              }
+          });
+      });
+        const mailData = {
+          from: 'Mehdigital@outlook.com', // sender address
+          to: 'Mehdigital@outlook.com', // list of receivers
+          subject: "Portfolio PHOTO", // Subject line
+          text: `Name:${Nom} Prenom:${Prenom} Email:${Email} Message:${Message} Tel:${Tel}`, // plain text body
+          html: `<span>Name: ${Nom}</span><br><span>Prenom: ${Prenom}</span><br><span>Email: ${Email}</span><br><span>Message: ${Message}</span></span><br><span>Tel: ${Tel}</span>`, // html body
         }
+        // send mail with defined transport object
+        await new Promise((resolve,reject) => 
+         transporter.sendMail({
+          from: 'Mehdigital@outlook.com', // sender address
+          to: 'Mehdigital@outlook.com', // list of receivers
+          subject: "Portfolio PHOTO", // Subject line
+          text: `Name:${Nom} Prenom:${Prenom} Email:${Email} Message:${Message} Tel:${Tel}`, // plain text body
+          html: `<span>Name: ${Nom}</span><br><span>Prenom: ${Prenom}</span><br><span>Email: ${Email}</span><br><span>Message: ${Message}</span></span><br><span>Tel: ${Tel}</span>`, // html body
+        },(err,info) => {
+          if(err){
+            console.log(err);
+            reject(err)
+          }
+          else {
+            
+            resolve(info)
+          }
+         }))
+      
        
         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
       
        
+      }
       
-      
-     
-  
-   export default sendEmail
-  
+      main().catch(console.error);
+
+    let {Nom,Prenom,Email,Tel,Message} = req.body;
+   res.status(200).json({status:"OK"}) ;
 
 
 
-  
+  }
 
