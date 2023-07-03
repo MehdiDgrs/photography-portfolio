0cecb706dd052f62a48c09c1cb342d5ac2469c36
MehdiDgrs <Mehdigital@outlook.com>
2023-07-03 15:53:19 +0200

    changed mail provider

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,20 +1,20 @@
-const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+var postmark = require("postmark");
 
+// Send an email:
+var client = new postmark.ServerClient("54437e2f-bb77-4cce-92bc-9f896b73679e");
 export default async (req, res) => {
   let { Nom, Prenom, Email, Tel, Message } = req.body;
 
-  let msg = {
-    from: "obelisque59@gmail.com", // sender address
-    to: "obelisque59@gmail.com", // list of receivers
-    subject: "Portfolio PHOTO", // Subject line
-    text: `Name:${Nom} Prenom:${Prenom} Email:${Email} Message:${Message} Tel:${Tel}`, // plain text body
-    html: `<span>Name: ${Nom}</span><br><span>Prenom: ${Prenom}</span><br><span>Email: ${Email}</span><br><span>Message: ${Message}</span></span><br><span>Tel: ${Tel}</span>`, // html body
-  };
-
   try {
-    let sendedMsg = await sgMail.send(msg);
-    console.log(sendedMsg);
+    client.sendEmail({
+      From: "photographe@mehdidegryse-photographe-lille.com",
+      To: "photographe@mehdidegryse-photographe-lille.com",
+      Subject: "Hello from Postmark",
+      HtmlBody: `<span>Name: ${Nom}</span><br><span>Prenom: ${Prenom}</span><br><span>Email: ${Email}</span><br><span>Message: ${Message}</span></span><br><span>Tel: ${Tel}</span>`,
+      TextBody: `Name:${Nom} Prenom:${Prenom} Email:${Email} Message:${Message} Tel:${Tel}`,
+      MessageStream: "outbound",
+    });
+
     console.log("Email sent successfully");
     res.status(200).json("");
   } catch (error) {
