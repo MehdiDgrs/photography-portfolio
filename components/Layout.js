5a9c6f10f53c4495ce707f310b8479cc4d3b0173
MehdiDgrs <Mehdigital@outlook.com>
2023-08-07 23:22:58 +0200

    robot and sitemap

--- components/Layout.js
+++ components/Layout.js
@@ -1,25 +1,31 @@
 import Header from "./Header";
 import Head from "next/head";
-import React from 'react'
+import React from "react";
 import { createContext } from "react";
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
+  return (
+    <div className="sm:flex">
+      <Head>
+        <title>
+          Mehdi Degryse Photographe Developpeur Web Front End Portfolio Lille
+        </title>
 
- 
-  
-    return ( 
-       
-        <div className="sm:flex">
-        <Head>
-    <title>Mehdi Degryse  Photographe Developpeur Web Front End Portfolio Lille</title>
-    <meta name="description" content="Portfolio photographe developpeur web front end haut de france Lille " />
-    <link rel="icon" href="/favicon.ico" />
-  </Head>
-        <Header/>
-        
-        <main id="main" className="w-full sm:w-4/6  sm:flex-col sm:p-5 md:w-5/6 md:flex-col md:p-5" >{children}</main>
-        </div>
-       
-       
-    )
+        <meta name="robots" content="all" />
+        <meta
+          name="description"
+          content="Portfolio photographe developpeur web front end haut de france Lille "
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+
+      <main
+        id="main"
+        className="w-full sm:w-4/6  sm:flex-col sm:p-5 md:w-5/6 md:flex-col md:p-5"
+      >
+        {children}
+      </main>
+    </div>
+  );
 }
